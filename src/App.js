1fe2765dc26c1b271f04bd857ba9396a96c17c4b
Josh Gearheart <other.josh.gearheart@gmail.com>
2019-06-01 16:46:45 -0700

    build header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { createStore, applyMiddleware,  } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
+
+import Header from './components/Header'
 
 require('dotenv').config();
 
@@ -19,21 +21,9 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>poopoo</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+        <div className="container">
+          <Header/>
+          <p>Poop.</p>
         </div>
       </Provider>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import '../styles/Header.scss';
+
+export default function Header() {
+  return (
+    <div className="gradient wide100">
+      <div className="headerItem">
+        <h1>Jonah.</h1>
+        <h1>Behance.</h1>
+        <h1>Etsy.</h1>
+        <h1>Contact</h1>
+      </div>
+      <div className="headerItem">
+        <h3>Garbage.</h3>
+      </div>
+    </div>
+  )
+}
